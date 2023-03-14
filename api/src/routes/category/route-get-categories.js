4b76df20ce1cd2ceb6df096f46758ea536f78708
soyfriss <federico.rissone@gmail.com>
2023-03-14 11:10:56 -0300

    refactor(back/categories): use express-validator

--- api/src/routes/category/route-get-categories.js
+++ api/src/routes/category/route-get-categories.js
@@ -2,19 +2,4 @@ const { Router } = require('express');
 const { getCategories } = require('../../controllers/category/controller-get-categories');
 
 // GET /categories
-const router = Router();
-router.get(
-    '/',
-    async (req, res, next) => {
-        try {
-            const { page, size, sort, filter } = req.query;
-            const categories = await getCategories(page, size, sort, filter);
-
-            res.status(200).json(categories);
-        } catch (error) {
-            next(error)
-        }
-    }
-);
-
-module.exports = router;
+module.exports = Router().get('/', getCategories);
--- api/src/routes/category/route-post-category.js
+++ api/src/routes/category/route-post-category.js
@@ -1,21 +1,12 @@
 const { Router } = require('express');
 const { createCategory } = require('../../controllers/category/controller-post-category');
+const validationRules = require('../../controllers/category/validation-rules');
+const validateRules = require('../../middlewares/validate-rules');
 
-const router = Router();
-
-// POST /categories
-router.post(
-    '/',
-    async (req, res, next) => {
-        try {
-            const { name } = req.body;
-            const category = await createCategory(name);
-
-            res.status(200).json(category);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-module.exports = router;
+module.exports = Router()
+    .post(
+        '/',
+        validationRules(),
+        validateRules,
+        createCategory
+    );
