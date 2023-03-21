8aff89237aa597dc3a39c122ac2d8a14d76fc493
NRotili <64414877+NRotili@users.noreply.github.com>
2023-03-21 19:10:07 -0300

    refactor(front/employees): reuse form (#31)

--- client/src/admin/employee/employee-create.jsx
+++ client/src/admin/employee/employee-create.jsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
-import { Create, SimpleForm, TextInput, SelectInput, useRedirect, useNotify, useCreate } from 'react-admin';
-import { Grid } from '@mui/material';
+import { Create, SimpleForm, useRedirect, useNotify, useCreate } from 'react-admin';
+import { EmployeeForm }  from './employee-form';
 
 export const EmployeeCreate = () => {
     const redirect = useRedirect();
@@ -42,65 +42,7 @@ export const EmployeeCreate = () => {
     return (
         <Create>
             <SimpleForm onSubmit={save}>
-                    <Grid container columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-                        <Grid item xs={12} md={4} sm={12}   >
-                            <TextInput
-                                required
-                                fullWidth
-                                label="First Name"
-                                source="firstname"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Last Name"
-                                source="lastname"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Email Address"
-                                source="email"
-                                type='email'
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Phone"
-                                source="phone"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                source="filenumber"
-                                label="File Number"
-                                type="number"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <SelectInput 
-                                source="status"
-                                label="Status"
-                                optionValue="name"
-                                defaultValue={"Active"}
-                                choices={[
-                                    { id: "Active", name: "Active" },
-                                    { id: "Disabled", name: "Disabled" }
-                                ]}
-                                required
-                                fullWidth
-                            />
-                        </Grid>
-                    </Grid>
+                    <EmployeeForm/>
             </SimpleForm>
         </Create>
     );
--- client/src/admin/employee/employee-edit.jsx
+++ client/src/admin/employee/employee-edit.jsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
-import { Edit, SimpleForm, TextInput, Toolbar, SaveButton, useRedirect, useNotify, useUpdate, SelectInput } from 'react-admin';
-import { Grid } from '@mui/material';
+import { Edit, SimpleForm, Toolbar, SaveButton, useRedirect, useNotify, useUpdate } from 'react-admin';
+import { EmployeeForm } from './employee-form';
 
 // Custom toolbar without <DeleteButton>
 const PostEditToolbar = props => (
@@ -57,65 +57,7 @@ export const EmployeeEdit = () => {
             <SimpleForm
                 toolbar={<PostEditToolbar />}
                 onSubmit={save}>
-                <Grid container columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-                        <Grid item xs={12} md={4} sm={12}   >
-                            <TextInput
-                                required
-                                fullWidth
-                                label="First Name"
-                                source="firstname"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Last Name"
-                                source="lastname"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Email Address"
-                                source="email"
-                                type='email'
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                label="Phone"
-                                source="phone"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <TextInput
-                                required
-                                fullWidth
-                                source="filenumber"
-                                label="File Number"
-                                type="number"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={4} sm={12}>
-                            <SelectInput 
-                                source="status"
-                                label="Status"
-                                optionValue="name"
-                                defaultValue={"Active"}
-                                choices={[
-                                    { id: "Active", name: "Active" },
-                                    { id: "Disabled", name: "Disabled" }
-                                ]}
-                                required
-                                fullWidth
-                            />
-                        </Grid>
-                    </Grid>
+                    <EmployeeForm/>
             </SimpleForm>
         </Edit>
     );
