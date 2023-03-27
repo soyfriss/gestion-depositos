24d5d606ade33a9979bce36b1949490f886c1e5f
NRotili <64414877+NRotili@users.noreply.github.com>
2023-03-27 13:17:51 -0300

    feat(backend/user): get user by id or username
    by Id: /users/{id}
    by UserName: /users/user/{username}

--- api/src/routes/user/index.js
+++ api/src/routes/user/index.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const getUsers = require('./route-get-users');
+const getUser = require('./route-get-user');
+const getUserByUserName = require('./route-get-user-by-username');
 
 const router = Router();
 router.use('/', getUsers);
+router.use('/', getUser);
+router.use('/user', getUserByUserName);
 
 module.exports = router;
