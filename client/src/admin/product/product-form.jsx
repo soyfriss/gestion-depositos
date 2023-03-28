7521d8b02a27d77d592dae7a65eaaa64c1428190
soyfriss <federico.rissone@gmail.com>
2023-03-28 20:17:02 -0300

    feat(frontend/product): crud

---
+++ client/src/admin/product/product-form.jsx
@@ -0,0 +1,48 @@
+import { TextInput, SelectInput, ReferenceArrayInput, SelectArrayInput } from "react-admin";
+import { Grid } from '@mui/material'
+
+export const ProductForm = () => {
+    return <>
+        <Grid container columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+            <Grid item xs={12} sm={12} md={6}>
+                <TextInput
+                    required
+                    fullWidth
+                    label="Name"
+                    source="name"
+                    autoFocus
+                />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+                <SelectInput
+                    required
+                    fullWidth
+                    label="Status"
+                    source="status"
+                    optionValue="name"
+                    defaultValue="Active"
+                    choices={[
+                        { id: "Active", name: "Active" },
+                        { id: "Active", name: "Active" }
+                    ]}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextInput
+                    fullWidth
+                    multiline
+                    label="Description"
+                    source="description"
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <ReferenceArrayInput required reference="categories" source="categories">
+                    <SelectArrayInput
+                        optionText="name"
+                        fullWidth
+                    />
+                </ReferenceArrayInput>
+            </Grid>
+        </Grid>
+    </>;
+}
