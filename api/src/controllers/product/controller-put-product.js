2cfd874bac4070aac55cf60afacaa4f7b256142a
soyfriss <federico.rissone@gmail.com>
2023-03-29 13:32:47 -0300

    fix(backend/product): validations

--- api/src/controllers/product/controller-put-product.js
+++ api/src/controllers/product/controller-put-product.js
@@ -1,26 +1,37 @@
-const { Product } = require('../../db');
+const { Product, conn } = require('../../db');
 const httpStatusCodes = require('../../utils/http-status-codes');
 const constants = require('../../utils/constants');
 
 const editProduct = async (req, res, next) => {
     try {
         const { id } = req.params;
-        const { name, description, stock, status } = req.body;
+        const { name, description, stock, status, categories } = req.body;
 
-        const product = await Product.findByPk(id);
+        await conn.transaction(async (t) => {
+            const product = await Product.findByPk(id, { transaction: t });
+            const productCategories = await product.getCategories({ transaction: t });
 
-        if (!product) {
-            res.status(httpStatusCodes.BAD_REQUEST).json({ error: constants.ITEM_NOT_FOUND });
-        }
+            if (!product) {
+                res.status(httpStatusCodes.BAD_REQUEST).json({ error: constants.ITEM_NOT_FOUND });
+            }
 
-        await product.update({
-            name,
-            description,
-            stock,
-            status
+            await product.update({
+                name,
+                description,
+                stock,
+                status
+            },
+                { transaction: t }
+            );
+
+            // Edit categories
+            await product.removeCategories(productCategories, { transaction: t });
+            if (categories && categories.length) {
+                await product.addCategories(categories, { transaction: t });
+            }
+
+            res.status(httpStatusCodes.OK).json(product);
         });
-
-        res.status(httpStatusCodes.OK).json(product);
     } catch (error) {
         next(error);
     }
