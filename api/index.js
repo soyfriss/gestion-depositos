67ed6c48b3800f999920a3f94e41f61d795ea740
soyfriss <federico.rissone@gmail.com>
2023-05-12 14:26:06 -0300

    git push --all --tags --forcefeat(infra): initial commit

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const { conn } = require('./src/db.js');
 const setInitialData = require('./src/utils/initial-data');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  setInitialData();
+conn.sync({ alter: true }).then(() => {
+  // setInitialData();
   server.listen(3001, () => {
     console.log('Server listening at 3001'); // eslint-disable-line no-console
   });
